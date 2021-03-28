19b81ec086f9f3ccf04bb4272cc9349a27e7fd64
leonor93 <victor.romero2016@uteq.edu.ec>
2021-03-28 18:28:10 -0500

    posi

---
+++ js/JS.js
@@ -0,0 +1,36 @@
+
+
+var idsubir;
+let  contador = 0;
+
+function  pag()
+{
+    location.href = "../WISHES/Company.html";
+}
+function  dale(ids)
+{
+
+    
+
+        if (ids === "2") {
+           
+                document.body.scrollTop = document.documentElement.scrollTop = 1250;
+          
+        } else if (ids === "3")
+        {
+           
+                document.body.scrollTop = document.documentElement.scrollTop =600;
+        }
+        if (ids === "1")
+        {
+           
+                document.body.scrollTop = document.documentElement.scrollTop = 1600;
+        }
+    
+}
+
+
+function bb()
+{
+      document.body.scrollTop = document.documentElement.scrollTop =300;
+}
